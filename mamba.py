1157be08806ac48e4eb2b4cad7b2675c7891f56a
DTadpole <gsk390@gmail.com>
2024-09-16 04:38:59 +0000

    mlp

--- mamba.py
+++ mamba.py
@@ -3,45 +3,75 @@ import torch.nn.functional as F
 from dataclasses import dataclass
 from mamba_ssm import Mamba
 from mamba_ssm.ops.triton.layer_norm import RMSNorm, layer_norm_fn
-from gpt2 import LayerNorm
+from gpt2 import LayerNorm, MLP
 
 @dataclass
 class MyMambaConfig:
     block_size: int = 1024
     vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
-    n_layer: int = 12
+    n_layer: int = 6
     d_model: int = 768
+    d_ffn: int = 768 * 4
     d_state: int = 16
     d_conv: int = 4
     n_expand: int = 2
+    dropout: float = 0.1
     conv_bias: bool = False
     bias: bool = False # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    enable_mlp: bool = True
 
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc    = nn.Linear(config.d_model, config.d_ffn, bias=config.bias)
+        self.silu    = nn.SiLU()
+        self.c_proj  = nn.Linear(config.d_ffn, config.d_model, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.silu(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
+
+
+
+class MyBlock(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln1 = LayerNorm(config.d_model, bias=config.bias)
+        self.mamba = Mamba(d_model=config.d_model, d_state=config.d_state, d_conv=config.d_conv,
+                  expand=config.n_expand, conv_bias=config.conv_bias, bias=config.bias)
+        self.ln2 = LayerNorm(config.d_model, bias=config.bias)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.mamba(self.ln1(x))
+        x = x + self.mlp(self.ln2(x))
+        return x
 
 
 class MyMamba(nn.Module):
 
     def __init__(self, config):
         super().__init__()
+        self.config = config
         self.emb = nn.Embedding(config.vocab_size, config.d_model)
         self.head = nn.Linear(config.d_model, config.vocab_size, bias=False)
         self.blocks = nn.ModuleList()
         for _ in range(config.n_layer):
-            block = nn.ModuleList()
-            block.append(LayerNorm(config.d_model, bias=config.bias))
-            block.append(Mamba(d_model=config.d_model, d_state=config.d_state, d_conv=config.d_conv,
-                  expand=config.n_expand, conv_bias=config.conv_bias, bias=config.bias))
-            self.blocks.append(block)
+            self.blocks.append(MyBlock(config))
 
     def forward(self, x, y):
         # apply embedding
         emb = self.emb(x)
         # apply blocks
         for block in self.blocks:
-            orig = emb
-            for m in block:
-                emb = m(emb)
-            emb = emb + orig
+            emb = block(emb)
 
         logits = self.head(emb)  # Changed from x to emb
         loss = F.cross_entropy(logits.view(-1, logits.size(-1)), y.view(-1), ignore_index=-1)
